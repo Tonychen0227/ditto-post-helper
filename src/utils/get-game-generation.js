962db2f61a81610cbfc87c32ebdcd38f4e6b1eda
ShinySylveon04 <carlotta.basketball04@gmail.com>
2020-02-23 16:50:51 -0600

    Add SwSh check

--- src/utils/get-game-generation.js
+++ src/utils/get-game-generation.js
@@ -1,1 +1,6 @@
-export const getGameGen = game => (["ORAS", "XY"].includes(game) ? 6 : 7);
+export const getGameGen = game =>
+  ["ORAS", "XY"].includes(game)
+    ? 6
+    : ["Sword", "Shield"].includes(game)
+    ? 8
+    : 7;
